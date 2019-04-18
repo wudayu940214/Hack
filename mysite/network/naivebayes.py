d09fcdd19a698b9b4f26359caa1c3e97ad0222b6
dang lin <dang.lin@oracle.com>
2019-04-19 07:49:00 +0800

    no message

--- mysite/network/naivebayes.py
+++ mysite/network/naivebayes.py
@@ -32,9 +32,9 @@ def load_dataset():
 	class_vec = []
 	listP = []
 	listN = []
-	for i in range(500):
+	for i in range(5000):
 		class_vec.append(i%2)
-	for i in range(200):
+	for i in range(2000):
 		strP=pos.readline()
 		strN=neg.readline()
 		regP=re.compile('\\W*')
@@ -49,8 +49,8 @@ def load_dataset():
 		if len(strN) > 0:
 			sent_list.append([strN])
 			listN.append([strN])
-	print(sent_list)
-	print(class_vec)
+	#print(sent_list)
+	#print(class_vec)
 	pos.close()
 	neg.close()
 	return sent_list, class_vec, listP, listN
@@ -59,7 +59,7 @@ def create_vocab_list(dataset):
 	vocab_set = set([])
 	for doc in dataset:
 		vocab_set = vocab_set | set(doc)
-	print(vocab_set)
+	#print(vocab_set)
 	return list(vocab_set)
  
 def set_of_words2vec(vocab_list, input_set):
@@ -69,13 +69,13 @@ def set_of_words2vec(vocab_list, input_set):
 		if word in vocab_list:
 			return_vec[vocab_list.index(word)] = 1
 
-	print(return_vec)
+	#print(return_vec)
 	return return_vec
  
 def trainNB(train_matrix, train_catagory):
 	num_train_docs = len(train_matrix)
-	print(num_train_docs)
-	print(len(train_catagory))
+	#print(num_train_docs)
+	#print(len(train_catagory))
 	num_words = len(train_matrix[0])
 	pos_num = 0
 	for i in train_catagory:
@@ -118,7 +118,7 @@ def responseLoad(request):
 		Jsonresult = sentiment_analysis("the", "", "")
 		return JsonResponse(Jsonresult, safe = False)
 	else:
-		return show_wordcloud()
+		return show_wordcloud("the", "", "")
 
 # def queryText(keywords, name, period, index="twitter_data", doc_type="test_type", ip="148.70.167.220"):
 #     es = Elasticsearch(ip)
@@ -171,7 +171,8 @@ def sentiment_analysis(keyword, name, period):
 	for sent_in_doc in list_sents:
 		train_mat.append(set_of_words2vec(my_vocab_list, sent_in_doc))
 	p0V, p1V, pAb = trainNB(train_mat, list_classes)
-	#test_entry1 = ['It', 'is', 'better']
+	test_entry1 = ['It', 'is', 'better']
+	print(classifyNB(np.array(set_of_words2vec(my_vocab_list, test_entry1)), p0V, p1V, pAb))
 	#test_entry2 = ['abort', 'me']
 	#test_context = set([])
 	textContentList = queryES(keyword, name, period)
@@ -180,12 +181,18 @@ def sentiment_analysis(keyword, name, period):
 	# print(str(textContentList))
 	for line in textContentList:
 		print("sdfdsfsdfsdfsf" + str(line))
-		# reg = re.compile('\\W*')
-		# solveData = reg.split(line['_source']['text'])
+		#reg = re.compile('\\W*')
+		#solveData = reg.split(line['_source']['text'])
+		solvedata = line['_source']['text']
+		#print(solvedata)
+		alldata = solvedata.lower().split()
+		#print(alldata)
 		# print("wangbadanwangbadan"+solveData[0])
-		solveData=json.loads(json.dumps(line))
-		result =classifyNB(np.array(set_of_words2vec(my_vocab_list, solveData['_source']['text'])), p0V, p1V, pAb)
-		# print(result)
+		#RAWdata=json.loads(json.dumps(line))['_source']['text']
+		#solveData=reg.split(json.loads(json.dumps(line))['_source']['text'])
+		#print(solveData)
+		result =classifyNB(np.array(set_of_words2vec(my_vocab_list, alldata)), p0V, p1V, pAb)
+		print(result)
 		positiveCount = 0
 		negativeCount = 0
 		neutralCount = 0
@@ -197,65 +204,69 @@ def sentiment_analysis(keyword, name, period):
 			neutralCount += 1
 		if positiveCount > negativeCount:
 			analysisResult = "positive"
-			print(listP)
-			for word in listP:
-				mydictP = dict()
-				if word in solveData:
-					mydictP[word] += 1
-			#sorted_wordP = sorted(mydictP.items(), key=itemgetter(1), reverse=True)
-			sorted_wordP = str(sorted(mydictP.items(), key=itemgetter(1), reverse=True))
-			print("-----------------------------------"+sorted_wordP)
-			sorted_wordP = re.sub(r'\[', "{", sorted_wordP)
-			sorted_wordP = re.sub(r'\]', "}", sorted_wordP)
-			sorted_wordP = re.sub(r'\'\,', "':", sorted_wordP)
-			sorted_wordP = re.sub(r'\(|\)', "", sorted_wordP)
+		# 	#print(listP)
+		# 	for word in listP:
+		# 		mydictP = dict()
+		# 		#print(word)
+		# 		if str(word) in alldata:
+		# 			mydictP[word] += 1
 			
-			indexP  = 1
-			for keyP in sorted_wordP:
-				if indexP == 1:
-					wordP1 = keyP
-					print(sorted_wordP[keyP])
-					wordP1Value = int(sorted_wordP[keyP])
-					indexP += 1
-					continue
-				elif indexP == 2:
-					wordP2 = keyP
-					wordP2Value = int(sorted_wordP[keyP])
-					indexP += 1
-					continue
-				else:
-					wordP3 = keyP
-					wordP3Value = int(sorted_wordP[keyP])
-			otherCountP = sum(sorted_wordP.values()) - sorted_wordP[wordP1] - sorted_wordP[wordP2] - sorted_wordP[wordP3]
+		# 	#sorted_wordP = sorted(mydictP.items(), key=itemgetter(1), reverse=True)
+		# 	print("---------------"+str(mydictP))
+		# 	sorted_wordP = str(sorted(mydictP.items(), key=itemgetter(1), reverse=True))
+		# 	#print("-----------------------------------"+sorted_wordP)
+		# 	sorted_wordP = re.sub(r'\[', "{", sorted_wordP)
+		# 	sorted_wordP = re.sub(r'\]', "}", sorted_wordP)
+		# 	sorted_wordP = re.sub(r'\'\,', "':", sorted_wordP)
+		# 	sorted_wordP = re.sub(r'\(|\)', "", sorted_wordP)
+		# 	print(sorted_wordP)
+		# 	sorted_wordP = eval(sorted_wordP)
+		# 	indexP  = 1
+		# 	for keyP in sorted_wordP:
+		# 		if indexP == 1:
+		# 			wordP1 = keyP
+		# 			#print(sorted_wordP[keyP])
+		# 			wordP1Value = int(sorted_wordP[keyP])
+		# 			indexP += 1
+		# 			continue
+		# 		elif indexP == 2:
+		# 			wordP2 = keyP
+		# 			wordP2Value = int(sorted_wordP[keyP])
+		# 			indexP += 1
+		# 			continue
+		# 		else:
+		# 			wordP3 = keyP
+		# 			wordP3Value = int(sorted_wordP[keyP])
+		# 	otherCountP = sum(sorted_wordP.values()) - sorted_wordP[wordP1] - sorted_wordP[wordP2] - sorted_wordP[wordP3]
 		elif negativeCount > positiveCount:
-			analysisResult = "negative"
-			for word in listN:
-				mydictN = dict()
-				if word in solveData:
-					mydictN[word] += 1
-			#sorted_wordN = sorted(mydictN.items(), key=itemgetter(1), reverse=True)
-			sorted_wordN = str(sorted(mydictN.items(), key=itemgetter(1), reverse=True))
-			sorted_wordN = re.sub(r'\[', "{", sorted_wordN)
-			sorted_wordN = re.sub(r'\]', "}", sorted_wordN)
-			sorted_wordN = re.sub(r'\'\,', "':", sorted_wordN)
-			sorted_wordN = re.sub(r'\(|\)', "", sorted_wordN)
-			print(sorted_wordN)
-			indexN = 1
-			for keyN in range(3):
-				if indexN == 1:
-					wordN1 = keyN
-					wordN1Value = int(sorted_wordN[keyN])
-					indexN +=1
-					continue
-				elif indexN == 2:
-					wordN2 = keyN
-					wordN2Value = int(sorted_wordN[keyN])
-					indexN +=1
-					continue
-				else:
-					wordN3 = keyN
-					wordN3Value = int(sorted_wordN[keyN])
-			otherCountN = sum(sorted_wordP.values() - sorted_wordP[wordN1] - sorted_wordP[wordN2] - sorted_wordP[wordN3])
+				analysisResult = "negative"
+		# 	for word in listN:
+		# 		mydictN = dict()
+		# 		if word in solveData:
+		# 			mydictN[word] += 1
+		# 	#sorted_wordN = sorted(mydictN.items(), key=itemgetter(1), reverse=True)
+		# 	sorted_wordN = str(sorted(mydictN.items(), key=itemgetter(1), reverse=True))
+		# 	sorted_wordN = re.sub(r'\[', "{", sorted_wordN)
+		# 	sorted_wordN = re.sub(r'\]', "}", sorted_wordN)
+		# 	sorted_wordN = re.sub(r'\'\,', "':", sorted_wordN)
+		# 	sorted_wordN = re.sub(r'\(|\)', "", sorted_wordN)
+		# 	print(sorted_wordN)
+		# 	indexN = 1
+		# 	for keyN in range(3):
+		# 		if indexN == 1:
+		# 			wordN1 = keyN
+		# 			wordN1Value = int(sorted_wordN[keyN])
+		# 			indexN +=1
+		# 			continue
+		# 		elif indexN == 2:
+		# 			wordN2 = keyN
+		# 			wordN2Value = int(sorted_wordN[keyN])
+		# 			indexN +=1
+		# 			continue
+		# 		else:
+		# 			wordN3 = keyN
+		# 			wordN3Value = int(sorted_wordN[keyN])
+		# 	otherCountN = sum(sorted_wordP.values() - sorted_wordP[wordN1] - sorted_wordP[wordN2] - sorted_wordP[wordN3])
 		else:
 			analysisResult = "netural"
 	# 	result =classifyNB(np.array(set_of_words2vec(my_vocab_list, test_entry1)), p0V, p1V, pAb)
@@ -332,13 +343,9 @@ def sentiment_analysis(keyword, name, period):
 		"result": analysisResult,
 		"positive": {
 			"total": positiveCount,
-			"words": [wordP1, wordP2, wordP3, "others"],
-			"counts": [int(wordP1Value), int(wordP2Value), int(wordP3Value), int(otherCountP)]
 		},
 		"negative": {
 			"total": negativeCount,
-			"words": [wordN1, wordN2, wordN3, "others"],
-			"counts": [int(wordN1Value), int(wordN2Value), int(wordN3Value0), int(otherCountN)]
 		},
 		"netural": {
 			"total": neutralCount
@@ -350,23 +357,27 @@ def sentiment_analysis(keyword, name, period):
 	#print(classifyNB(np.array(set_of_words2vec(my_vocab_list, test_entry2)), p0V, p1V, pAb))
 
 
-def show_wordcloud():
-    wordcloud = WordCloud(
+def show_wordcloud(keywword, name, period):
+	context = queryES("the", "John", period)
+	result = ''
+	for line in context:
+		solvedata = line['_source']['text']
+		result = result + ' ' + solvedata
+	wordcloud = WordCloud(
         background_color='white',
         stopwords=stopwords,
         max_words=200,
         max_font_size=40, 
         scale=3,
         random_state=1 # chosen at random by flipping a coin; it was heads
-    ).generate(str("data matt"))
-
-    fig = plt.figure()
-    plt.axis('off')
-    plt.imshow(wordcloud, interpolation="bilinear")
-    wordcloud.to_file(os.path.join(module_dir, "rtrwa.jpeg"))
-    f = open(os.path.join(module_dir, "rtrwa.jpeg"), 'rb')
-    base64_data = base64.b64encode(f.read())
-    return HttpResponse(base64_data, content_type='image/jpeg')
+    ).generate(str(result))
+	fig = plt.figure()
+	plt.axis('off')
+	plt.imshow(wordcloud, interpolation="bilinear")
+	wordcloud.to_file(os.path.join(module_dir, "rtrwa.jpeg"))
+	f = open(os.path.join(module_dir, "rtrwa.jpeg"), 'rb')
+	base64_data = base64.b64encode(f.read())
+	return HttpResponse(base64_data, content_type='image/jpeg')
 
 
 def index(request):
